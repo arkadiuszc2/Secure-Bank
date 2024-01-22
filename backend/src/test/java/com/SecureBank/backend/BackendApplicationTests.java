db1ed54d69d098cfb816c1397d92d813fd97bf73
Arkadiusz Ciepliński <arkadiusz.cieplinski2@gmail.com>
2024-01-22 11:42:05 +0100

    Initialize backend project, add basic config

---
+++ backend/src/test/java/com/SecureBank/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SecureBank.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
